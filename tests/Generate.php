a55e39c409a5c22b157180b580caacb09805ed8f
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-09-16 16:55:58 -0400

    Create Generate.php

---
+++ tests/Generate.php
@@ -0,0 +1,22 @@
+<?php
+require 'src/hashFile/hashFile.php';
+// Place base directory between apostrophes or leave empty for /
+$hashFile = new hashFile('');
+function getDirectory() {
+    echo PHP_EOL . "\e[1;32mGet Hashes for Directory or File:\e[0m ";
+    $handle = fopen ("php://stdin","r");
+    $line = fgets ($handle);
+    $dir = trim($line);
+    if (!is_dir($dir) && !is_file($dir)) {
+        echo PHP_EOL . "\e[1;91mInvalid Directory!\e[0m";
+        $$dir = getDirectory();
+    }
+    return $dir;
+}
+$dir = getDirectory();
+if (is_dir($dir)) $hashFile->hashDirectoryFiles($dir);
+if (is_file($dir)) $hashFile->hashFile($dir);
+$hashes = $hashFile->getHashes();
+foreach ($hashes as $file => $hash) {
+    echo '\'' . $file . '\' => \'' . $hash . '\',' . PHP_EOL;
+}
